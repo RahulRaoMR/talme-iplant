875dba1ce956c7fdd6409dafbd1537131beafe58
RahulRaoMR <royalrahulraomr9103@gmail.com>
2026-08-06 10:53:33 +0530

    Fix Vercel API rewrite routing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 const { requestHandler } = require("../src/server");
 
 module.exports = async function handler(req, res) {
+  const rewritePath = req.query?.path;
+  if (rewritePath && new URL(req.url, "http://localhost").pathname === "/api") {
+    const segments = Array.isArray(rewritePath) ? rewritePath : String(rewritePath).split("/");
+    const url = new URL(req.url, "http://localhost");
+    url.searchParams.delete("path");
+    const query = url.searchParams.toString();
+    req.url = `/api/${segments.map(segment => encodeURIComponent(segment)).join("/")}${query ? `?${query}` : ""}`;
+  }
   return requestHandler(req, res);
 };
